acd190883ae10dd6e774d8277a27f0f075024deb
Alvaro Rios <alvaro@smartsites.cl>
2018-07-26 21:08:25 -0400

    adding links to menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :todos
+
   get 'todos', to: 'todos#index'
   get 'todos/new'
+  get 'todos/list'
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get 'todos/:id/complete', to: 'todos#complete', as: 'complete'
+
+  root 'todos#index'  
 end
